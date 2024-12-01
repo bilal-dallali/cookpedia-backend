37c989eb6ee318f2771a29df0742a73178688457
bilal-dallali <dallali-bilal@hotmail.fr>
2024-12-01 23:25:34 +0100

    no more 404 message

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ db.connect((err) => {
     }
 });
 
-app.use((req, res, next) => {
-    res.status(404).json({ error: "Route not found" });
-});
-
 //const usersRoute = require("./routes/users.js")
 app.use("/api/users", usersRoute)
 app.use("/api/recipes", recipesRoute)
