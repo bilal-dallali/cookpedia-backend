15c269f4fa33c1636b8b3c57c5bfafe873c64115
bilal-dallali <dallali-bilal@hotmail.fr>
2024-12-01 23:08:26 +0100

    anyway

--- config/db.js
+++ config/db.js
@@ -7,7 +7,9 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    uri: process.env.DB_URI,
+    port: process.env.DB_PORT
 });
 
 export default db;
