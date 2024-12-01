c9022896f34d4f8d392a910abad43829e567c93f
bilal-dallali <dallali-bilal@hotmail.fr>
2024-12-01 21:51:19 +0100

    adjustments

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ db.connect((err) => {
     }
 });
 
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Route not found" });
+});
+
 //const usersRoute = require("./routes/users.js")
 app.use("/api/users", usersRoute)
 app.use("/api/recipes", recipesRoute)
